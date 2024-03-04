50f1dedb598d3d0d334d3dbdcd6d335f7144a921
Andrew January <ajanuary@evertz.com>
2024-03-04 10:31:29 +0000

    Mention RCE works on mobile

--- site/web/stream/index.php
+++ site/web/stream/index.php
@@ -12,7 +12,7 @@ render_header();
 
 <article>
   <h3>Stream and catch-up</h3>
-  <p>We are using a platform called Ring Central Events to stream our programme items. You can watch the live streams of programme items, or watch the recorded videos afterwards.</p>
+  <p>We are using a platform called Ring Central Events to stream our programme items. You can watch the live streams of programme items, or watch the recorded videos afterwards. It works on both desktop and mobile.</p>
   <p><a class="button" target="_blank" href="<?php echo $magic_link; ?>">Go to Ring Central Events</a></p>
   <p><em>This link is tied to your membership. Please do not share it with other people.</em></p>
   <p>Thank you to <a href="https://glasgow2024.org/">Glasgow 2024</a> for donating the use of their Ring Central Events account for our convention.</p>
