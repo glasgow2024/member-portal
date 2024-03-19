335decb92714d78f9086268b5c1e38b04f27a86a
Andrew January <ajanuary@evertz.com>
2024-03-19 16:52:28 +0000

    Improve error handling in chat callback

--- site/web/chat_callback.php
+++ site/web/chat_callback.php
@@ -6,6 +6,10 @@ require_once('../includes/session_auth.php');
 require_once('../includes/requests.php');
 
 $code = $_GET['code'];
+if (!$code) {
+  error_log("No code in chat_callback.php: " . print_r($_GET, true));
+  die("Sorry, something went wrong. Please try again. If the problem persists, contact <a href=\"mailto:" . EMAIL . "?subject=Problem with member portal&body=Problem: No code in chat_callback.php\">". EMAIL . "</a>");
+}
 
 // Exchange code for access token
 $token_resp = api_call('https://discord.com/api/oauth2/token', [
@@ -19,6 +23,10 @@ $token_resp = api_call('https://discord.com/api/oauth2/token', [
   'scope' => 'identify'
 ]));
 $access_token = $token_resp['access_token'];
+if (!$access_token) {
+  error_log("Missing access_token: " . print_r($token_resp, true));
+  die("Sorry, something went wrong. Please try again. If the problem persists, contact <a href=\"mailto:" . EMAIL . "?subject=Problem with member portal&body=Problem: No access token in chat_callback.php\">". EMAIL . "</a>");
+}
 
 // Store username
 $me_resp = api_call('https://discord.com/api/users/@me', [
@@ -27,6 +35,11 @@ $me_resp = api_call('https://discord.com/api/users/@me', [
 
 $discord_id = $me_resp['id'];
 $discord_username = $me_resp['username'];
+
+if (!$discord_id || !$discord_username) {
+  error_log("Missing discord_id or discord_username: " . print_r($me_resp, true));
+  die("Sorry, something went wrong. Please try again. If the problem persists, contact <a href=\"mailto:" . EMAIL . "?subject=Problem with member portal&body=Problem: No Discord ID or username in chat_callback.php\">". EMAIL . "</a>");
+}
 
 db_set_discord_id(get_current_user_badge_no(), $discord_id, $discord_username);
 
@@ -40,6 +53,11 @@ $invite_resp = api_call('https://discord.com/api/channels/' . DISCORD_CHANNEL_ID
   'unique' => true
 ]));
 $invite_code = $invite_resp['code'];
+
+if (!$invite_code) {
+  error_log("Missing invite_code: " . print_r($invite_resp, true));
+  die("Sorry, something went wrong. Please try again. If the problem persists, contact <a href=\"mailto:" . EMAIL . "?subject=Problem with member portal&body=Problem: No invite code in chat_callback.php\">". EMAIL . "</a>");
+}
 
 header('Location: https://discord.gg/' . $invite_code);
 
